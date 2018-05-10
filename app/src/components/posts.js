7d39999ad370ec5cfe024dacbfd39a1cd169507c
Adan <twinadanz@gmail.com>
2018-05-10 14:05:50 -0500

    set api data to component state

--- app/src/components/posts.js
+++ app/src/components/posts.js
@@ -12,6 +12,7 @@ class Posts extends Component { // eslint-disable-line react/prefer-stateless-fu
       fetch('https://jsonplaceholder.typicode.com/posts') // This is the API call. Will fetch dummy data first,
         .then(res => res.json()) // then , a promise is created to map the data into a json object...
         .then(data => console.log(data)) // and finally, console.log the data.
+        .then(data => this.setState({ posts: data })) // and finally, send our data to our component state.
   }
 
   render() {
