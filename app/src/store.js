ff865e84d0335133eae12749b575c7094c9364f1
Adan <twinadanz@gmail.com>
2018-05-10 16:48:49 -0500

    add redux chrome extension support

--- app/src/store.js
+++ app/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 //Reducers
@@ -9,9 +9,12 @@ const intitalState = [];
 const middleWare = [thunk];
 
 const store = createStore(
-    rootReducer,
-    intitalState,
-    applyMiddleware(...middleWare)
+    rootReducer, // Combined reducers
+    intitalState, // initialState
+    compose(
+        applyMiddleware(...middleWare), // Enhancer functions
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 export default store;
