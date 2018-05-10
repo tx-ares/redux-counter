b659e9169e739dcca2e24e8548aec686d538805a
Adan <twinadanz@gmail.com>
2018-05-10 14:56:38 -0500

    create blank store and imports

--- app/src/App.js
+++ app/src/App.js
@@ -2,22 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+
 //Components
 import Posts from './components/posts';
 import PostForm from './components/postform';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+        <Provider store={store}>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <PostForm />
+            <hr />
+            <Posts />
+          </div>
+        </Provider>
     );
   }
 }
