9e7aee106fe435cb1ed2e67f22d1f80e18cf7481
Adan <twinadanz@gmail.com>
2018-05-10 14:05:37 -0500

    Create component level state

--- app/src/components/posts.js
+++ app/src/components/posts.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Posts extends Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) { // This is ALWAYS required to create a component level state.  Remember this.
+      super(props); //
+      this.state = { //
+          posts: [] //
+      } //
+  } // - All this.
+
   componentWillMount() {
       fetch('https://jsonplaceholder.typicode.com/posts') // This is the API call. Will fetch dummy data first,
         .then(res => res.json()) // then , a promise is created to map the data into a json object...
