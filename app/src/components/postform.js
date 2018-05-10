21c0ef419c1ea1fa1b344b13230405bb08efe0c0
Adan <twinadanz@gmail.com>
2018-05-10 14:17:22 -0500

    create form markup

--- app/src/components/postform.js
+++ app/src/components/postform.js
@@ -12,7 +12,19 @@ class PostForm extends Component { // eslint-disable-line react/prefer-stateless
 
     return (
     <div>
-      <h1>Add Post</h1>
+        <h1>Add Post</h1>
+        <form>
+            <div>
+                <label>Title: </label> <br />
+                <input type="text" name="title" />
+            </div>
+            <div>
+                <label>Body: </label> <br />
+                <textarea name="body" />
+            </div> <br />
+
+            <button type="submit">Submit </button>
+        </form>
     </div>
     );
   }
